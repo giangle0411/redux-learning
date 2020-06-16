5428ed892c4e0be700ff238b39c4edef9d71c154
Giang-Le <giangle0411@gmail.com>
2020-06-16 21:26:00 +1000

    New Blog app, redux-thunk and jsonPlaceholder API

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,10 @@
+import jsonPlaceholder from '../apis/jsonPlaceholder'
+
 // Action creator
-export const selectSong = (song) => {
-  return {
-    type: 'SONG_SELECTED',
-    payload: song,
+export const fetchPosts = () => {
+  return async (dispatch) => {
+    const response = await jsonPlaceholder.get('/posts')
+
+    dispatch({ type: 'FETCH_POSTS', payload: response })
   }
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,22 +1,6 @@
 import { combineReducers } from 'redux'
-
-const songsReducer = () => {
-  return [
-    { title: 'Houdini', duration: '3:22' },
-    { title: 'Salad Days', duration: '2:26' },
-    { title: 'The World Is Watching', duration: '3:36' },
-    { title: 'Sunflower', duration: '4:12' },
-  ]
-}
-
-const selectedSongReducer = (selectedSong = null, action) => {
-  if (action.type === 'SONG_SELECTED') {
-    return action.payload
-  }
-  return selectedSong
-}
+import postReducer from './postReducer'
 
 export default combineReducers({
-  songs: songsReducer,
-  selectedSong: selectedSongReducer,
+  post: postReducer,
 })
