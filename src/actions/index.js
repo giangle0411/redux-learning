df83aa93fbc6f55e27b5942a8d9edcabda3f8d11
Giang-Le <giangle0411@gmail.com>
2020-06-17 18:35:06 +1000

    Lodash Memoize

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,8 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder'
 
 // Action creator
+import _ from 'lodash'
+
 export const fetchPosts = () => {
   return async (dispatch) => {
     const response = await jsonPlaceholder.get('/posts')
@@ -8,3 +10,15 @@ export const fetchPosts = () => {
     dispatch({ type: 'FETCH_POSTS', payload: response.data })
   }
 }
+
+//two methods are the same for action creators
+
+export const fetchUser = (id) => (dispatch) => {
+  _fetchUser(id, dispatch)
+}
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
+  const response = await jsonPlaceholder.get(`/users/${id}`)
+
+  dispatch({ type: 'FETCH_USER', payload: response.data })
+})
